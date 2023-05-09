bc4fb6f9ae1e606fbcc024007c3c6015b0abec91
zhuxining <zhuxining@outlook.com>
2023-05-09 17:34:49 +0800

    add index

--- app/items_example/model.py
+++ app/items_example/model.py
@@ -1,7 +1,9 @@
-from beanie import Document
+from beanie import Document, Indexed
 from typing import Any, Set, Union
 from uuid import UUID, uuid4
 from pydantic import Field
+import pymongo
+from pymongo import IndexModel
 
 
 class Item(Document):
@@ -13,3 +15,19 @@ class Item(Document):
     tax: Union[float, None] = None
     tags: Set[str] = set()
     image: Any = None
+
+    class Settings:
+        name = "item"
+        indexes = [
+            IndexModel([("uid", pymongo.DESCENDING)], unique=True),
+            IndexModel([("num", pymongo.ASCENDING)], unique=False),
+            IndexModel([("name", pymongo.ASCENDING)], unique=True),
+            IndexModel(
+                [
+                    ("name", pymongo.TEXT),
+                    ("description", pymongo.TEXT),
+                    ("tags", pymongo.TEXT),
+                ],
+                name="text_index",
+            ),
+        ]
