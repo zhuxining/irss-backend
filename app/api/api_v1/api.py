a6983dd5ae56fb303e893458a4d4e100d9d79520
zhuxining <zhuxining@outlook.com>
2023-05-17 15:44:27 +0800

    del feed example

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import auth, feeds, item, users
+from app.api.api_v1.endpoints import users, item
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(feeds.router, prefix="/feeds", tags=["feeds"])
 api_router.include_router(item.router, prefix="/example", tags=["example"])
